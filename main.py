9e58f90abaaef569897071f881c5fc67d64055d0
Ólafur Aron Jóhannsson <olafur@johannsson.co>
2016-07-16 22:49:39 +0000

    start message

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from crawler import WebCrawler
+import datetime
 
 # runner file
 
 if __name__ == '__main__':
+    search_url = argv[1] or 'https://github.com'
+    print('WebCrawler started, scanning {0}'.format(search_url))
     # initiate a crawl, the crawler encapsulates the event loop
-    c = WebCrawler('https://github.com')
+    c = WebCrawler(search_url)
     c.crawl()
